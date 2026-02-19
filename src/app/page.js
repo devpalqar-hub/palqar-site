8beb7b58c9d1b8c11b64bb5c2e44fb52ca01fb63
devpalqar-hub <dev.palqar@gmail.com>
2026-02-19 18:51:20 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
+import FeaturedProjects from "@/components/sections/FeaturedProjects/FeaturedProjects";
 import Hero from "@/components/sections/Hero/Hero";
 import ServicesPreview from "@/components/sections/Services/ServicesPreview";
+import WhySection from "@/components/sections/WhySection/WhySection";
 
 export default function Home() {
   return (
     <main >
       <Hero/>
       <ServicesPreview/>
+      <FeaturedProjects/>
+      <WhySection/>
     </main>
   );
 }
